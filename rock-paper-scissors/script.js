12235ca48ed89fb702001914fc853a2f43017f69
zyaka <vlagover@gmail.com>
2021-12-05 15:19:28 -0500

    made the game functional

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,4 @@
-const playObjects = ["Rock", "Paper", "Scissors"];
+const playObjects = ["rock", "paper", "scissors"];
 const random = Math.floor(Math.random() * playObjects.length);
 
 function computerPlay() {
@@ -6,9 +6,38 @@ function computerPlay() {
 }
 
 console.log(computerPlay());
-let playerSelection;
-let computerSelection;
+const playerSelection = prompt("enter an object:", "");
+console.log(playerSelection);
+const computerSelection = computerPlay();
+
 
 function playRound(computerSelection, playerSelection) {
+    debugger;
+    if (computerSelection === playerSelection) {
+        return "It's a tie!";
+    } else if (computerSelection === "rock") {
+        if (playerSelection === "paper") {
+            return "You win! Paper beats rock";
+        } else {
+            return "You lose! Rock beats scissors";
+        }
+    } else if (computerSelection === "paper") {
+        if (playerSelection == "rock") {
+            return "You lose! Paper beats rock";
+        } else {
+            return "You win! Scissors beat paper";
+        }
+    } else if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            return "You win! Rock beats scissors";
+        } else {
+            return "You lose! Scissors beat paper";
+        }
+    } else {
+        return "Something's not right.."
+    }
+
 
 }
+
+console.log(playRound(computerSelection, playerSelection));
